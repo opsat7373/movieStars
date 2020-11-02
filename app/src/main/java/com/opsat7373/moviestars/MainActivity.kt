35b074fa63672b0cdd1834892d8f4c9922d37472
vkorovay <vkorovay@mindk.com>
2020-11-03 00:29:55 +0200

    inti commit

---
+++ app/src/main/java/com/opsat7373/moviestars/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.opsat7373.moviestars
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
